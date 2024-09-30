643106d0f1a61e71e119b9fe18c36f4fa42160ef
Adarsh Dhakar <adarshdhakar277@gmail.com>
2024-09-30 23:02:18 +0530

    removed

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/saas";
+const MONGO_URL = "mongodb+srv://chitchat:ChitChat%402004@cluster0.wu40t.mongodb.net/saas?retryWrites=true&w=majority&appName=Cluster0";
 
 main()
     .then(() => {
