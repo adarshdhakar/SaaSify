c35543d099fdb557a2b903793d0fea89cea00c6b
Adarsh Dhakar <adarshdhakar277@gmail.com>
2024-10-03 23:37:28 +0530

    few bugs

--- controllers/users.js
+++ controllers/users.js
@@ -14,11 +14,9 @@ module.exports.signup = async (req, res) => {
         req.login(registeredUser, (err) => {
             if (err) return next(err);
 
-            req.flash("success", "Welcome to Wanderlust!");
+            req.flash("success", "Welcome to SaaSify!");
             res.redirect("/listings");
         });
-        // req.flash("success", "Welcome to Wanderlust!");
-        // res.redirect("/listings");
     }
     catch(e){
         req.flash("error", e.message);
@@ -31,8 +29,7 @@ module.exports.renderLoginPage = (req, res) => {
 };
 
 module.exports.login = async(req, res) => {
-    // res.send("Welcome to Wanderlust! You are logged in!");
-    req.flash("success", "Welcome back to Wanderlust!");
+    req.flash("success", "Welcome back to SaaSify!");
     let redirectUrl = res.locals.redirectUrl || "listings";
     res.redirect(redirectUrl);  
 };
